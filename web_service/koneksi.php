d22c9629670be117524f21c073569a4b45eb822f
Haykal12345678 <55924437+Haykal12345678@users.noreply.github.com>
2020-04-04 03:02:02 +0700

    Add files via upload

---
+++ web_service/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+ define('HOST','localhost');
+ define('USER','id12797004_haykalpt');
+ define('PASS','haykal12');
+ define('DB','id12797004_akademik');
+
+ $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
